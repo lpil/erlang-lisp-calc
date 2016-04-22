b19aa97e90a7bcb7a93e0dd6b0db0fb80a06889b
Louis Pilfold <louis@lpil.uk>
2016-04-22 08:34:06 +0100

    Lex numbers

--- src/calc_lexer.xrl
+++ src/calc_lexer.xrl
@@ -2,10 +2,12 @@ Definitions.
 
 Num = [0-9]
 WS  = [\n\s\r\t]
+OP  = [/*+-]
 
 Rules.
 
 {Num}+   : {token, {number, as_int(TokenChars)}}.
+{OP}     : {token, {op, list_to_atom(TokenChars)}}.
 {WS}     : skip_token.
 
 Erlang code.
--- src/calc_lexer_test.erl
+++ src/calc_lexer_test.erl
@@ -6,7 +6,10 @@
 tokenizer_test_() ->
   [?_assertEqual(
       [{number, 123}],
-      tokens("123"))].
+      tokens("123")),
+   ?_assertEqual(
+      [{op, '+'}, {op, '-'}, {op, '*'}, {op, '/'}],
+      tokens("+ - * /"))].
 
 tokens(S) ->
   {ok, T, _} = calc_lexer:string(S),
