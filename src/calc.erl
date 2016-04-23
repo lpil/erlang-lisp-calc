483f1a30f6bd824daf640aaeb5734a392e6d1500
Louis Pilfold <louis@lpil.uk>
2016-04-23 22:56:36 +0100

    Basic evaluation

--- src/calc.erl
+++ src/calc.erl
@@ -1,24 +1,83 @@
 -module(calc).
+-export([evaluate/1]).
 
--include_lib("eunit/include/eunit.hrl").
+%% Public
 
-%% API exports
--export([hello/0]).
+evaluate(Code) ->
+  {ok, Tokens, _} = calc_lexer:string(Code),
+  {ok, Tree} = calc_parser:parse(Tokens),
+  {ok, eval(Tree)}.
 
-%%====================================================================
-%% API functions
-%%====================================================================
+%% Private
 
-hello() ->
-  "Hello, world!".
+eval([Fn|Args]) ->
+  eval_sexp(Fn, Args);
+eval([]) -> 0;
+eval(X)  -> X.
+
+
+eval_sexp('*', Args) ->
+  Mult = fun(X, Y) -> X * Y end,
+  lists:foldl(Mult, 1, Args);
+
+eval_sexp('/', [Arg]) ->
+  1 / Arg;
+eval_sexp('/', [Hd|Tl]) ->
+  Div = fun(X, Y) -> Y / X end,
+  lists:foldl(Div, Hd, Tl);
+
+eval_sexp('+', [Hd|Tail]) ->
+  Add = fun(X, Y) -> X + Y end,
+  lists:foldl(Add, Hd, Tail);
+
+eval_sexp('-', [Arg]) ->
+  0 - Arg;
+eval_sexp('-', [Hd|Tail]) ->
+  Sub = fun(X, Y) -> Y - X end,
+  lists:foldl(Sub, Hd, Tail).
 
 
 -ifdef(TEST).
-eunit_test_() ->
-  [?_assertEqual(2, 1 + 1),
-   ?_assertEqual("hello", "hello")].
--endif.
+-include_lib("eunit/include/eunit.hrl").
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+basic_types_test_() ->
+  [?_assertEqual(
+      {ok, 1},
+      evaluate("1")),
+   ?_assertEqual(
+      {ok, 0},
+      evaluate("()"))].
+
+addition_test_() ->
+  [?_assertEqual(
+      {ok, 99},
+      evaluate("(+ 99)")),
+   ?_assertEqual(
+      {ok, 15},
+      evaluate("(+ 5 5 5)"))].
+
+subtraction_test_() ->
+  [?_assertEqual(
+      {ok, -10},
+      evaluate("(- 10 20)")),
+   ?_assertEqual(
+      {ok, -15},
+      evaluate("(- 15)"))].
+
+multiplication_test_() ->
+  [?_assertEqual(
+      {ok, 15},
+      evaluate("(* 15)")),
+   ?_assertEqual(
+      {ok, 144},
+      evaluate("(* 12 12 1)"))].
+
+division_test_() ->
+  [?_assertEqual(
+      {ok, 0.1},
+      evaluate("(/ 10)")),
+   ?_assertEqual(
+      {ok, 2.0},
+      evaluate("(/ 20 5 2)"))].
+
+-endif.
